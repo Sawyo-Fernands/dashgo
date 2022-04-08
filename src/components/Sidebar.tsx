5602f2e661dbd5f5086660baeb3d7e35fba7e374
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-04-08 09:28:50 -0300

    Componente: SideBar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { Box, Stack, Text,Link, Icon } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
+
+
+
+export function SideBar(){
+    return(
+        <Box as="aside" width='64' mr='8'>
+            <Stack spacing='12' align='flex-start'>
+                <Box>
+                    <Text fontWeight='bold' color='gray.400' fontSize='small'>
+                        GERAL
+                    </Text>
+                    <Stack spacing='12' mt='8' align='stretch'>
+                        <Link display='flex' alignItems='center'  py='1'>
+                            <Icon as={RiDashboardLine} fontSize='20'/>
+                            <Text ml='4' fontWeight='medium'>Dashboard</Text>
+                        </Link>
+                        <Link display='flex' alignItems='center'  py='1'>
+                            <Icon as={RiContactsLine} fontSize='20'/>
+                            <Text ml='4' fontWeight='medium'>Usuários</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+                <Box>
+                    <Text fontWeight='bold' color='gray.400' fontSize='small'>
+                        AUTOMAÇÂO
+                    </Text>
+                    <Stack spacing='12' mt='8' align='stretch'>
+                        <Link display='flex' alignItems='center'  py='1'>
+                            <Icon as={RiInputMethodLine} fontSize='20'/>
+                            <Text ml='4' fontWeight='medium'>Formulários</Text>
+                        </Link>
+                        <Link display='flex' alignItems='center'  py='1'>
+                            <Icon as={RiGitMergeLine} fontSize='20'/>
+                            <Text ml='4' fontWeight='medium'>Automação</Text>
+                        </Link>
+                    </Stack>
+                </Box>
+            </Stack>
+
+        </Box>
+    )
+}
