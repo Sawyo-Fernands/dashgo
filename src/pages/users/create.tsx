e33c4125b36c8504d6dbf176c6280c7610ccaa12
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-04-08 21:23:16 -0300

    Componentizando Pagination

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,6 +1,3 @@
-
-
-
 import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack} from "@chakra-ui/react";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
+import { Pagination } from "../../components/Pagination/page";
 import { SideBar } from "../../components/SideBar";
 
 
