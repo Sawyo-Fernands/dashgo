e47ee44599173de4e9fc652246fefadd8a943e56
Sawyo Fernands <78683353+Sawyo-Fernands@users.noreply.github.com>
2022-04-15 11:48:54 -0300

    Sinalizando refetch dos dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,7 +10,7 @@ import { useQuery } from 'react-query'
 
 export default function UserList(){
 
-    const { data,isLoading,error }=useQuery('users',async ()=>{
+    const { data,isLoading,error,isFetching }=useQuery('users',async ()=>{
 
     const response=   await fetch('http://localhost:3000/api/users')
      const data=  await response.json()   
@@ -52,7 +52,11 @@ export default function UserList(){
                 <Box flex={'1'} borderRadius={8} bgColor='gray.800' p={'8'}>
 
                     <Flex mb={8} justify='space-between' alignItems={'center'}>
-                        <Heading size={'lg'} fontWeight='normal'>Usuários</Heading>
+                        <Heading size={'lg'} fontWeight='normal'>Usuários {
+                            !isLoading && isFetching  && (
+                                <Spinner size={'sm'} color='gray.500' ml={4}/>
+                            )
+                        }</Heading>
                         <Link href={'/users/create'} passHref>
                         <Button as={'a'} size='sm' fontSize={'15'} 
                         colorScheme='purple' leftIcon={<Icon as={RiAddLine}/>} cursor='pointer'>
